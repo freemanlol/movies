aa794ae365da56b823607562aead49e4a9781144
Freeman <48633960+freemanlol@users.noreply.github.com>
2020-06-08 01:47:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navClass: ['slider__nav--left', 'slider__nav--right'],
+    responsive:{
+        0:{
+            items:1
+        },
+        700:{
+            items:2
+        },
+        900:{
+            items:3
+        }
+    }
+})
+});
